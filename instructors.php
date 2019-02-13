6ed0043c4a6174b7701071e9925fee3c1d8d7c73
rahulbshrestha <ra.shrestha@jacobs-university.de>
2019-02-13 22:38:02 +0100

    Removing html

--- instructors.php
+++ instructors.php
@@ -1,20 +1,20 @@
 <!DOCTYPE html>
-   <html lang="en">
-   <head>
-      <meta charset="utf-8">
-    <title>Students</title>
+	<html lang="en">
+  	<head>
+   	<meta charset="utf-8">
+    <title>Instructors</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/style.css">    
+    <link rel="stylesheet" href="css/style.css">
     <script src = "js/scripts.js"></script>
   </head>
-  
+
   <body>
     <!-- Header section starts -->
-    <header id="home" class="home-area">    
+    <header id="home" class="home-area">
       <div class="overlay"></div>
       <nav class="navbar navbar-expand-md bg-inverse fixed-top scrolling-navbar">
         <div class="container">
-          <a href="index.html" class="navbar-brand"><img src="images/logo.png" alt=""></a>  
+          <a href="index.html" class="navbar-brand"><img src="images/logo.png" alt=""></a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
             <i class="lni-menu"></i>
           </button>
@@ -25,58 +25,50 @@
               </li>
               <li class="nav-item">
                 <a class="nav-link page-scroll" href="students.html">Students</a>
-              </li>  
+              </li>
               <li class="nav-item">
                 <a class="nav-link page-scroll" href="courses.html">Courses</a>
-              </li>                            
+              </li>
               <li class="nav-item">
                 <a class="nav-link page-scroll" href="instructors.html">Instructors</a>
               </li>
               <li class="nav-item">
                 <a class="nav-link page-scroll" href="#Discussion">Discussion</a>
-              </li> 
+              </li>
               <li class="nav-item">
                 <a class="nav-link page-scroll" href="#help">Help</a>
               </li>
               <li class="nav-item">
                 <a class="nav-link page-scroll" href="imprint.html">Imprint</a>
-              </li>  
+              </li>
             </ul>
           </div>
         </div>
-      </nav>  
-      <div class="container">      
+      </nav>
+      <div class="container">
         <div class="row space-100">
           <div class="col-lg-7 col-md-12 col-xs-12">
             <div class="contents">
-              <h2 class="head-title">Students Sign Up Form </h2>
+              <h2 class="head-title">Instructors Input Form </h2>
               <form action="/action_page.php" method = "post">
-              First Name:<br>
+              First name:<br>
               <input type="text" name="firstname"><br>
-              Last Name<br>
+              Last name:<br>
               <input type="text" name="lastname"><br>
-              Major:<br>
-              <input type="text" name="major"><br>
-              Year:<br>
-              <input type="text" name="year"><br>
+              Department:<br>
+              <input type="text" name="department"><br>
               <br>
-
-              <input type="submit" value="Submit">
+              <input type="submit" value="submit" onclick = "return submitData();" >
             </form>
-
-
             </div>
-          </div>  
+          </div>
           <div class="col-lg-5 col-md-12 col-xs-12">
             
-
-
-         </div>            
+            </div>
           </div>
-  
-       
-      </div>             
+        </div>
+      </div>
     </header>
-    <!-- Header Section End --> 
+    <!-- Header Section End -->
   </body>
 </html>
