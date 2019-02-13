54a6a3fb14ae08e6d0cbd993e12a65823712756f
rahulbshrestha <ra.shrestha@jacobs-university.de>
2019-02-13 17:40:02 +0100

    test

--- students.php
+++ students.php
@@ -1,11 +1,13 @@
 <?php
 // Port you need to connect with MariaDB
-$DB_HOST = '0.0.0.0:3306';
+$DB_HOST = 'localhost:3306';
 $UName = 'rashrestha';
 $Pass = 'YsSAm5Aa';
 $DB_Name = 'wry';
 
 $connect = mysqli_connect($DB_HOST, $UName, $Pass, $DB_Name) or die("Failed to
 connect to MySQL: " . mysqli_error());
+
+echo "HELLO RAHUL!!";
 ?>
 
